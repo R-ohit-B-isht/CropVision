fdb5c72e1a54a1a3b1cca4f01df3066844bf3263
Rohit Bisht <rbtunes0@gmail.com>
2023-02-05 05:47:20 +0000

    almost done

--- CPR/src/components/About/AboutCard.js
+++ CPR/src/components/About/AboutCard.js
@@ -16,16 +16,29 @@ function AboutCard() {
     setSource(urlx);
     setinputImage(file);
     console.log(file)
+    const formData = new FormData();
+    formData.append("sentFile", file);
+
+
     const response = await axios.post(
-      "https://54.89.179.149:8000/api/diseasedetection",
-      { 'sentFile':urlx },
-      {
+      "https://8000-rohitbisht-cropvision-3f570jojlkm.ws-us85.gitpod.io/api/diseasedetection/",
+      formData, {
         headers: {
-          "Content-Type": "mulitpart/form-data",
+          "Content-Type": "multipart/form-data",
         },
-      }
-    );
-    console.log(response)
+      })
+      .then((response) => {
+        let temData = response
+        console.log(temData.data.Disease_Name)
+        console.log(response)
+        console.log(response.Disease_Name)
+        setDisease(temData.data.Disease_Name.toUpperCase())
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+      // setDisease(response.json())
+     
 
   }
   const handleChoose = (e) => { 
@@ -39,7 +52,14 @@ function AboutCard() {
       
 
         {(inputImage == "") && <Card.Img style={{ height: "300px", width: "300px" }} variant="top" src={tempImg} />}
-        {(inputImage!="")&&<Card.Img style={{height:"300px",width:"300px"}} variant="top" src={source} />}
+        {(inputImage != "") && <div>
+          <Card.Img style={{ height: "300px", width: "300px" }} variant="top" src={source} />
+          { (!disease.length)&&<><div id="loadingUi" style={{ display: "block" }}>
+                <div class="ui active dimmer">
+                  <div class="ui indeterminate text loader">Loading Info</div>
+                </div>
+              </div></>}
+        </div>}
         <blockquote className="blockquote mb-0" data-aos="zoom-in">
           
           <row>
@@ -58,15 +78,20 @@ function AboutCard() {
       </Card.Body>
       <Card.Body>
         {(disease == "TOMATO LEAF MOLD") && <p>
+        <h3>TOMATO LEAF MOLD</h3>
+          <br/>
           Method 1:Let the plants air out or expose to dry air conditions.
           <br/>
           Method 2:Spray the plants with fungicide sprays (Recommended: Calcium chloride sprays) and make sure to cover all parts of the plant that is above ground, focusing specifically on the underside of leaves.</p>}
         {(disease == "STRAWBERRY LEAF SCORCH") && <p>
           Method 1:Let the plant dry out by altering the watering practices and allowing for dryer conditions.
           <br/>
-          Method 2:Treat the plant with organic fungicides. (Captan 50 WP & Copper compounds)\n\nNote:\nCan be prevented most effectively with the use of resistant varieties. (Allstar, Jewell, Midway & etc.).</p>}
+          Method 2:Treat the plant with organic fungicides. (Captan 50 WP & Copper compounds)
+          <br/>Note:Can be prevented most effectively with the use of resistant varieties. (Allstar, Jewell, Midway & etc.).</p>}
         
-          {(disease == "TOMATO MOSAIC VIRUS") && <p>
+        {(disease == "TOMATO MOSAIC VIRUS") && <p>
+        <h3>TOMATO MOSAIC VIRUS</h3>
+          <br/>
           Note that there is NO cure for this viral disease!
           <br />
           <br/>
@@ -80,6 +105,8 @@ function AboutCard() {
         </p>}
 
         {(disease == "CORN COMMON RUST") && <p>
+        <h3>CORN COMMON RUST</h3>
+          <br/>
           Method 1:\nPlant resistant hybrids such as commercial corn.
           <br />
           Method 2:Go for tillage and crop rotation
@@ -89,6 +116,8 @@ function AboutCard() {
         </p>}
 
         {(disease == "CORN GRAY LEAF SPOT") && <p>
+        <h3>CORN GRAY LEAF SPOT</h3>
+          <br/>
           Method 1:Go for crop rotation and tillage will help to reduce inoculum levels in surface residues.
           <br />
           Method 2:Plant resistant varieties.
@@ -97,7 +126,7 @@ function AboutCard() {
                     
         </p>}
         {(disease == "POTATO EARLY BLIGHT") && <p>
-          Note that there is NO cure for blight on plants.
+        <h3>Note that there is NO cure for blight on plants.</h3>
           <br /><br />Action 1:Remove and destroy all infected plant.
           <br />Action 2:Disinfect garden tools and equipment to prevent further spread of blight.
           <br />Action 3:Plant resistant varieties or practice crop rotation to prevent blight.
@@ -105,7 +134,7 @@ function AboutCard() {
         </p>}
 
         {(disease == "HEALTHY") && <p>
-          Your plant is healthy!
+          <h3>Your plant is healthy!</h3>
           <br />
         However, do remember to practice good farming habits as prevention is the best cure diseases!      
         </p>}
